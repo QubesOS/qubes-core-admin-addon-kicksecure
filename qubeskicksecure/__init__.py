95bb1ba82dc40be82fc6902fb12505cf2cfffe62
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2025-10-05 14:40:25 +0200

    Blacken the source

--- qubeskicksecure/__init__.py
+++ qubeskicksecure/__init__.py
@@ -18,38 +18,40 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
-'''qubes-core-admin extension for handling Kicksecure related settings'''
+"""qubes-core-admin extension for handling Kicksecure related settings"""
 
 import qubes.ext
 import qubes.vm.templatevm
 
+
 class QubesKicksecureExtension(qubes.ext.Extension):
-    '''qubes-core-admin extension for handling Kicksecure related settings'''
-    @qubes.ext.handler('domain-add', system=True)
+    """qubes-core-admin extension for handling Kicksecure related settings"""
+
+    @qubes.ext.handler("domain-add", system=True)
     def on_domain_add(self, app, _event, vm, **_kwargs):
-        '''Handle new AppVM created on kicksecure template and adjust its
+        """Handle new AppVM created on kicksecure template and adjust its
         default settings
-        '''
-        template = getattr(vm, 'template', None)
+        """
+        template = getattr(vm, "template", None)
         if template is None:
             return
 
-        if 'kicksecure' in template.features:
-            vm.tags.add('sdwdate-gui-client')
+        if "kicksecure" in template.features:
+            vm.tags.add("sdwdate-gui-client")
 
-    @qubes.ext.handler('features-request')
+    @qubes.ext.handler("features-request")
     def on_features_request(self, vm, _event, untrusted_features):
-        '''Handle kicksecure template advertising itself'''
+        """Handle kicksecure template advertising itself"""
         # Allow VM to advertise itself as kicksecure. But do not allow to drop
         #  that info on its own
         if not isinstance(vm, qubes.vm.templatevm.TemplateVM):
             return
-        if 'kicksecure' in untrusted_features:
-            vm.features['kicksecure'] = True
+        if "kicksecure" in untrusted_features:
+            vm.features["kicksecure"] = True
 
-    @qubes.ext.handler('domain-load')
+    @qubes.ext.handler("domain-load")
     def on_domain_load(self, vm, _event):
-        '''Retroactively add tags to kicksecure.'''
-        if hasattr(vm, 'template') and 'kicksecure' in vm.template.features:
-            if 'sdwdate-gui-client' not in vm.tags:
-                vm.tags.add('sdwdate-gui-client')
+        """Retroactively add tags to kicksecure."""
+        if hasattr(vm, "template") and "kicksecure" in vm.template.features:
+            if "sdwdate-gui-client" not in vm.tags:
+                vm.tags.add("sdwdate-gui-client")
